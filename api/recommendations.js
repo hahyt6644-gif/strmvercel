774b1b97a279b913a9d4b3d1e13f0d7d4d105b3c
hahyt6644-gif <hahyt6644@gmail.com>
2026-05-06 09:26:35 +0530

    Create recommendations.js

---
+++ api/recommendations.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  return res.status(200).json([
+    {
+      title: "Journey of Love 18+ (2023)",
+      genre: "Crime, Thriller",
+      duration: "2h 02min",
+      language: "Hindi + Malayalam",
+      url: "https://1024terabox.com/s/1GaSz2cA7nB3sJ8HkHflogg",
+      thumbnail: "https://i.ibb.co/rGNGrjTx/4ca454c4afa6.jpg"
+    },
+    {
+      title: "Vaazha",
+      genre: "Crime, Drama",
+      duration: "2h 01min",
+      language: "Hindi + Malayalam",
+      url: "https://1024terabox.com/s/1lmLbhspNu-wWCuEp9fxYvw",
+      thumbnail: "https://i.ibb.co/DgkQkyKp/f1775c5d8d4c.jpg"
+    }
+  ]);
+}
